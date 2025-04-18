d688af407fc62a1f1022a83f783b73aae5b2280a
Grentra Volta f <144815084+sanyoo21@users.noreply.github.com>
2025-04-18 07:29:48 +0700

    Add files via upload

---
+++ for you/Mikh.js
@@ -0,0 +1,38 @@
+function clicks() {
+  $("#div1").hide();
+  $("#div2").show();
+  $("#div2").addClass("fadeinv2");
+
+  var typed = new Typed(".katakata", {
+    strings: [
+      "Cella, Cie makin Tua, Cieeee, Cieeee",
+      "Cie Tua, Cie TUA :v, Gw gak yakin lu siap buat berada di umur segitu ",
+      "Tapi semoga, kita ketemu di umur selnajutnya yak",
+      "DOA kita buat lu, Semoga yang disemogakan dapat segera disemogakan ",
+      "Semoga yang diharapkan segera di, di, di , diapain aja deh",
+      "Doa nya lu isi aja deh sendiri ama Tuhan,,,,,,,",
+      "Kita Kurang Expert dibidang gini-ginian, ",
+      "Doa nya isi aja yak sendiri, gak usah malu, Kek sama siape aje....",
+      "Maaf kalau kadonya telat ya ",
+      "Maafin juga diri lu kalau ada salah ama kita",
+      "Terus Maafin juga, Maaf Banget bener dah ini",
+      "Maafkan semua yang lalu, Ampuni hati kecilmu",
+      "Luka-luka hilanglah luka, Biar tenteram yang berkuasa",
+      "(Harusnya di Part tadi ada lagu, Maafkan saya Noob)",
+      "(Ya intinya)",
+      "",
+      "kita Bingung mau kasih kado apaan,  karena kita bingung",
+      "Jadi kita beliin ini aja deh",
+      "",
+      "Tapi semoga cella gak baca sampai habis bukunya",
+      "",
+      "Agak Khawatir juga kita, kalau lu baca bukunya sampai habis",
+      "Sekali lagi HBD, Tuhan berkati selalu dalam segalanya",
+      "Bye-bye",
+      "Tamat",
+      "Lanjut Tahun depan kalau mau Part 2",
+      "",
+        ],
+    typeSpeed: 100,
+  });
+}
